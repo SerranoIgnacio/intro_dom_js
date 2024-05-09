41334839bdca74cafa03cac4063382e8d3ed103c
Ignacio Serrano <86433073+SerranoIgnacio@users.noreply.github.com>
2024-05-09 20:47:23 -0300

    Add files via upload

--- ejercicios_practica/2- ejercicio/js/script.js
+++ ejercicios_practica/2- ejercicio/js/script.js
@@ -16,9 +16,19 @@ document.getElementById("boton3").innerHTML = "CORTAR";
 
 /* 2)
 Obtener todos los botones utilizando la clase "btn",
-luego a todos los botones agregar la clase "agua" */
+luego a todos los botones agregar la clase "electricidad" */
 
-const boton = document.querySelector(".btn");
-boton.classList.add("agua");
-boton.classList.contains('agua');
-console.log(boton.classList.contains('agua'));
+const boton1 = document.getElementById("boton1");
+boton1.classList.add("electricidad");
+boton1.classList.contains('electricidad');
+console.log(boton1.classList.contains('electricidad'));
+
+const boton2 = document.getElementById("boton2");
+boton2.classList.add("electricidad");
+boton2.classList.contains('electricidad');
+console.log(boton2.classList.contains('electricidad'));
+
+const boton3 = document.getElementById("boton3");
+boton3.classList.add("electricidad");
+boton3.classList.contains('electricidad');
+console.log(boton3.classList.contains('electricidad'));
